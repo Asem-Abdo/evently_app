49198931eb6c2713b7e5668ba93c4b9999f13204
Asem <asemabdo2131999@gmail.com>
2025-12-09 02:49:00 +0200

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:evently/ui/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
